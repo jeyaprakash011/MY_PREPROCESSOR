30af43749508f0de9f465b8a468ca9c944e03782
JEYAPRAKASH <134125474+jeyaprakash011@users.noreply.github.com>
2026-08-18 18:24:09 +0530

    Add header file with data structure and function prototypes

---
+++ Input_Output_Files/headder.h
@@ -0,0 +1,16 @@
+#ifndef head
+#define head
+
+#include<stdio.h>
+#include<string.h>
+
+struct data
+       {
+           char name[50];
+           char value[100];
+       };
+int macro_rp(char *,char *,int);
+int remove_cmnd_line(char *,char *);
+int header_inc(char *,char *);
+    
+#endif
